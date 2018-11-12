abdb02570ff76619d8b09ffd39c0d02bf46c26ba
Owen Shepherd <414owen@gmail.com>
2018-11-12 01:01:57 +0000

    Cell type, Word8 usage

--- src/Interpret.hs
+++ src/Interpret.hs
@@ -5,20 +5,28 @@ module Interpret
   ) where
 
 import AST
+import Data.Word
 
-type Tape = ([Char], Char, [Char])
+type Cell = Word8
+type Tape = ([Cell], Cell, [Cell])
+
+getCell :: IO Cell
+getCell = fmap (toEnum . fromEnum) getChar
+
+putCell :: Cell -> IO ()
+putCell = putChar . (toEnum . fromEnum)
 
 exec :: AST -> Tape -> IO ()
-exec [] _                                      = pure ()
-exec (IncPtr         : nast) (l, c, (r : rs))  = exec nast (c : l, r, rs)
-exec (DecPtr         : nast) ((l : ls), c, r)  = exec nast (ls, l, c : r)
-exec (IncData        : nast) (l, c, r)         = exec nast (l, succ c, r)
-exec (DecData        : nast) (l, c, r)         = exec nast (l, pred c, r)
-exec (Output         : nast) tape@(l, c, r)    = putChar c >> exec nast tape
-exec (Input          : nast) (l, _, r)         = getChar >>= exec nast . (l,,r)
-exec (OpenCond goto  : nast) tape@(l, '\0', r) = exec goto tape
-exec (OpenCond goto  : nast) tape              = exec nast tape
-exec (CloseCond goto : nast) tape@(l, '\0', r) = exec nast tape
-exec (CloseCond goto : nast) tape              = exec goto tape
+exec [] _                                     = pure ()
+exec (IncPtr         : nast) (l, c, (r : rs)) = exec nast (c : l, r, rs)
+exec (DecPtr         : nast) ((l : ls), c, r) = exec nast (ls, l, c : r)
+exec (IncData        : nast) (l, c, r)        = exec nast (l, c + 1, r)
+exec (DecData        : nast) (l, c, r)        = exec nast (l, c - 1, r)
+exec (Output         : nast) tape@(l, c, r)   = putCell c >> exec nast tape
+exec (Input          : nast) (l, _, r)        = getCell >>= exec nast . (l,,r)
+exec (OpenCond goto  : nast) tape@(l, 0, r)   = exec goto tape
+exec (OpenCond goto  : nast) tape             = exec nast tape
+exec (CloseCond goto : nast) tape@(l, 0, r)   = exec nast tape
+exec (CloseCond goto : nast) tape             = exec goto tape
 
-interpret ast = exec ast (repeat '\0', '\0', repeat '\0')
+interpret ast = exec ast (repeat 0, 0, repeat 0)
