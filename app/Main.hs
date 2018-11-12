d78da819cbf1b1cc6e603c60dbc6d8370bbb9183
Owen Shepherd <414owen@gmail.com>
2018-11-12 00:30:44 +0000

    main takes program from file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
 import Lib
+import Data.Monoid
+import System.Environment
+import System.Exit
+import System.IO
 
 main :: IO ()
-main = getContents >>= runBF
+main = getArgs >>= \args ->
+  case args of
+    [] -> getProgName >>= \name ->
+      hPutStrLn stderr ("Usage: " <> name <> " <path>") >>
+      exitWith (ExitFailure 1)
+    (fname : _) -> readFile fname >>= runBF
