81e47cdf3262da85972f13d56cf2344ae78142d6
Owen Shepherd <414owen@gmail.com>
2018-11-12 00:36:35 +0000

    simplified Parse/addNode

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,29 +1,31 @@
-module Parse where
+module Parse
+  ( parse
+  ) where
 
 import AST
 import Data.Bifunctor
 
+addNode :: ASTNode -> (AST, [AST]) -> (AST, [AST])
+addNode node = first (node:)
+
+parseRec :: [AST] -> String -> (AST, [AST])
+parseRec _     []         = ([], [])
+parseRec conds ('>' : xs) = addNode IncPtr  $ parseRec conds xs
+parseRec conds ('<' : xs) = addNode DecPtr  $ parseRec conds xs
+parseRec conds ('+' : xs) = addNode IncData $ parseRec conds xs
+parseRec conds ('-' : xs) = addNode DecData $ parseRec conds xs
+parseRec conds ('.' : xs) = addNode Output  $ parseRec conds xs
+parseRec conds (',' : xs) = addNode Input   $ parseRec conds xs
+parseRec conds ('[' : xs) =
+  let recced@(nodes, futureConds) = parseRec (nodes : conds) xs in
+    case futureConds of
+      (futCond : ys) -> (OpenCond futCond : nodes, ys)
+      []             -> error "unmatched opening brace"
+parseRec []     (']' : xs) = error "unmatched closing brace"
+parseRec (y:ys) (']' : xs) =
+  let recced@(nodes, futureConds) = parseRec ys xs in
+    (CloseCond y : nodes, nodes : futureConds)
+parseRec conds (_ : xs) = parseRec conds xs
+
 parse :: String -> AST
 parse = fst . parseRec []
-  where
-    parseRec :: [AST] -> String -> (AST, [AST])
-    parseRec _     []         = ([], [])
-    parseRec conds ('>' : xs) = addNode IncPtr  $ parseRec conds xs
-    parseRec conds ('<' : xs) = addNode DecPtr  $ parseRec conds xs
-    parseRec conds ('+' : xs) = addNode IncData $ parseRec conds xs
-    parseRec conds ('-' : xs) = addNode DecData $ parseRec conds xs
-    parseRec conds ('.' : xs) = addNode Output  $ parseRec conds xs
-    parseRec conds (',' : xs) = addNode Input   $ parseRec conds xs
-    parseRec conds ('[' : xs) =
-      let recced@(nodes, futureConds) = parseRec (nodes : conds) xs in
-        case futureConds of
-          (futCond : ys) -> (OpenCond futCond : nodes, ys)
-          []             -> error "unmatched opening brace"
-    parseRec []     (']' : xs) = error "unmatched closing brace"
-    parseRec (y:ys) (']' : xs) =
-      let recced@(nodes, futureConds) = parseRec ys xs in
-        (CloseCond y : nodes, nodes : futureConds)
-    parseRec conds (_ : xs) = parseRec conds xs
-
-    addNode :: ASTNode -> (AST, [AST]) -> (AST, [AST])
-    addNode node (nodes, gotos) = (node : nodes, gotos)
